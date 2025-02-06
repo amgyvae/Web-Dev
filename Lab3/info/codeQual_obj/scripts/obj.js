611114b2a8051b60953efed190e85fb7f1bd0cdf
Margulan Sharipzhan <142485675+amgyvae@users.noreply.github.com>
2025-02-06 11:27:11 +0500

    Update obj.js

--- Lab3/info/codeQual_obj/scripts/obj.js
+++ Lab3/info/codeQual_obj/scripts/obj.js
@@ -1,1 +1,89 @@
+let user = {};
+user.name = "Mukhit";
+user.surname = "Nassyrov";
+user.name = "Kenes";
+delete user.name;
 
+function isEmpty(obj) {
+    for (let key in obj) return false;
+    return true;
+}
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
+let sum = 0;
+for (let key in salaries) {
+    sum += salaries[key];
+}
+
+multiplyNumeric = (obj) => {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+}
+function makeUser() {
+    return {
+        name: "John",
+        ref() {
+            return this;
+        }
+    };
+}
+let calculator = {
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    },
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+};
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep() {
+        alert( this.step );
+        return this;
+    }
+};
+
+let obj = {};
+
+function A() { return obj; }
+function B() { return obj; }
+function Calculator() {
+    this.read = function() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    };
+    this.sum = function() {
+        return this.a + this.b;
+    };
+    this.mul = function() {
+        return this.a * this.b;
+    };
+}
+function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = function() {
+        this.value += +prompt('How much to add?', 0);
+    };
+}
